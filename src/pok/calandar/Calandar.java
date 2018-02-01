ea6b6b1de54135dcc2fdee53c59d940edd239999
mm <mm@DESKTOP-KBPGBLI>
2018-02-01 22:27:45 +0900

    //두수를 키보드로 입력한다. 		//두수의 합읠 출력한다.

--- src/pok/calandar/Calandar.java
+++ src/pok/calandar/Calandar.java
@@ -1,12 +1,22 @@
 package pok.calandar;
 
+import java.util.Scanner;
+
 public class Calandar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Calandar");
-		System.out.println("복선미");
-
-
+		//두수를 키보드로 입력한다.
+		//두수의 합읠 출력한다.
+		
+		Scanner sc = new Scanner(System.in);
+		String param = sc.nextLine();
+		
+		String[] val = param.split(",");
+		
+		int val1 = Integer.parseInt(val[0]);
+		int val2 = Integer.parseInt(val[1]);
+		
+		System.out.println(val1+val2);
 	}
 
 }
