89d118163c21e919a39da2782d3113a1bbc0b50b
mm <mm@DESKTOP-KBPGBLI>
2018-02-01 22:16:01 +0900

    ㅇㅇㅇ

--- src/pok/calandar/Calandar.java
+++ src/pok/calandar/Calandar.java
@@ -3,8 +3,8 @@ package pok.calandar;
 public class Calandar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Calandar");
-		System.out.println("������");
+		System.out.println(Hello Calandar);
+		System.out.println(������);
 ㄷㄷㄷ
 
 	}
